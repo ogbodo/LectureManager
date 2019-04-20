1f6336cc916bc9f8f08bcb595bddc2f6a9971fa9
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-20 14:04:18 +0100

    reorganized code in user.js

--- User.js
+++ User.js
@@ -5,46 +5,46 @@ function User(name, role) {
   this.role = role || "Lecturer";
 
   console.log(this);
-
-  this.retrieveAllTimetable = function() {
-    return DB["timetable"];
-  };
-
-  this.retrieveLastTimetableId = function() {
-    const timetables = this.retrieveAllTimetable();
-
-    return timetables.length > 0 ? timetables[timetables.length - 1].id : 0;
-  };
-
-  this.readTimetableByCourseTitle = function(title) {
-    const timetables = this.retrieveAllTimetable();
-    var foundTimetables = [];
-    for (var i = 0; i < timetables.length; i++) {
-      if (timetables[i].title === title && this.role === "admin") {
-        foundTimetables.push(timetables[i]);
-      } else {
-        if (timetables[i].title === title && timetables[i].userId === this.id) {
-          foundTimetables.push(timetables[i]);
-        }
-      }
-    }
-    return foundTimetables.length > 0 || false;
-  };
-
-  this.readAllTimetables = function() {
-    const timetables = this.retrieveAllTimetable();
-    var lecturerTimetables = [];
-
-    for (var i = 0; i < timetables.length; i++) {
-      if (timetables[i].userId === this.id) {
-        lecturerTimetables.push(timetables[i]);
-      }
-    }
-
-    console.log(this.id + ": ", lecturerTimetables);
-
-    return lecturerTimetables;
-  };
 }
+
+User.prototype.retrieveAllTimetable = function() {
+  return DB["timetable"];
+};
+
+User.prototype.retrieveLastTimetableId = function() {
+  const timetables = this.retrieveAllTimetable();
+
+  return timetables.length > 0 ? timetables[timetables.length - 1].id : 0;
+};
+
+User.prototype.readTimetableByCourseTitle = function(title) {
+  const timetables = this.retrieveAllTimetable();
+  var foundTimetables = [];
+  for (var i = 0; i < timetables.length; i++) {
+    if (timetables[i].title === title && this.role === "admin") {
+      foundTimetables.push(timetables[i]);
+    } else {
+      if (timetables[i].title === title && timetables[i].userId === this.id) {
+        foundTimetables.push(timetables[i]);
+      }
+    }
+  }
+  return foundTimetables.length > 0 || false;
+};
+
+User.prototype.readAllTimetables = function() {
+  const timetables = this.retrieveAllTimetable();
+  var lecturerTimetables = [];
+
+  for (var i = 0; i < timetables.length; i++) {
+    if (timetables[i].userId === this.id) {
+      lecturerTimetables.push(timetables[i]);
+    }
+  }
+
+  console.log(this.id + ": ", lecturerTimetables);
+
+  return lecturerTimetables;
+};
 
 module.exports = User;
