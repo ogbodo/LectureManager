31d8ad1dc44afc4d7adfe58911734f25f62ac361
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-30 21:56:10 +0100

    Code optimization

--- interface.js
+++ interface.js
@@ -1,7 +1,11 @@
 function InheritProperty(childObject, sourceObject) {
-  var clonedObject = Object.create(sourceObject.prototype);
-  clonedObject.constructor = childObject;
-  childObject.prototype = clonedObject;
+  /**This is where the child class inherites from the parent class prototypically */
+  childObject.prototype = Object.create(sourceObject.prototype);
+
+  /**we will have to manually assign the constructor to the
+   *  child constructor because the child currently has the parent constructor.
+   * */
+  childObject.constructor = childObject;
 }
 
 module.exports = InheritProperty;
